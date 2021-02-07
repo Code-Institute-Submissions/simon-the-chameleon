01412c37d40769a371a6b74033f02956312e3aa9
belaventer <isaventer1@hotmail.com>
2021-02-07 17:55:23 +0000

    feat: Add Instructions content, style and temporary images

--- assets/js/simonCode.js
+++ assets/js/simonCode.js
@@ -1,3 +1,10 @@
+// On page load, display welcome content.
+$(document).ready(displayWelcomeContent());
+
+// Declaring project variables and local storage variables.
+var gameStarted;
+
+// Function to load Welcome content
 function displayWelcomeContent() {
     $("#game-playable-area").html(
         `<img class="image-simon flex-item-center" src="assets/images/simon-animation.gif" alt="Simon changing colours">
@@ -5,18 +12,62 @@ function displayWelcomeContent() {
         <p class="flex-item-center text-justify-center">How far can you follow Simon’s sequence?</p>`
     );
     $("#game-footer").html(
-        `<button onclick="displayInstructionsStepOne()">Instructions</button>
+        `<button onclick="displayInstructions(0)">Instructions</button>
         <button>Start</button>`
     );
+    
+    gameStarted = false;
 }
 
-function displayInstructionsStepOne() {
+// Function to load Instructions content
+function displayInstructions(stepNumber) {
+    var instructionsContent = [
+        ["instructionsStepOne", "starting the game", "Click on the START button below to start the game. Simon will change his color once the countdown completes."],
+        ["instructionsStepTwo", "your turn to play", "When Simon finishes his sequence, it is your turn to play. Click on the colours below Simon on the same order they were shown to move on to the next level"],
+        ["instructionsStepThree", "game over", "If you click the wrong colour, it is game over! But don't be sad, you can start a new game by clicking on the RESTART button"],
+        ["instructionsStepFour", "leaving or ending the game", "If you wish to stop the game and resume at a later day, click the SAVE & LEAVE button and your progress will be saved. If you wish to end the game to start a new run, click the END GAME button."],
+        ["instructionsStepFive", "sending your results to yourself", "Once the game is over, you can send the results to yourself by entering your email and clicking SEND"],
+        ["instructionsStepSix", "changing game settings", "You can change the game background colours by clicking on the settings button and chosing a diferent theme"]
+    ];
+
     $("#game-playable-area").html(
-        ``
+        `<h4 class="text-justify-center">Instructions</h4>
+        <div class="flex-container-row">
+            <p class="flex-item-center"><button onclick="checkStepNumber(${stepNumber}, 'Previous')"><i class="fas fa-long-arrow-alt-left"></i></button></p>
+            <img class="image-instruction" src="assets/images/${instructionsContent[stepNumber][0]}.png" alt="Simon game instruction: ${instructionsContent[stepNumber][1]}">
+            <p class="flex-item-center"><button onclick="checkStepNumber(${stepNumber}, 'Next')"><i class="fas fa-long-arrow-alt-right"></i></button></p>
+        </div>
+        <p class="text-justify-center">${instructionsContent[stepNumber][2]}</p>`
     );
+
+    var buttonString;
+
+    if (gameStarted == false) {
+        buttonString = "START"
+    } else {
+        buttonString = "CONTINUE"
+    };
+
+    $("#game-footer").addClass("flex-justify-center");
+
     $("#game-footer").html(
-        ``
+        `<button flex-item-center>${buttonString}</button>`
     );
 }
 
-$(document).ready(displayWelcomeContent());
+// Function to change the Instructions step displayed
+function checkStepNumber(stepNumber, changeDirection) {
+    if (stepNumber == 0 & changeDirection == "Previous") {
+        displayInstructions(5);
+        stepNumber = 5;
+    } else if (stepNumber == 5 & changeDirection == "Next") {
+        displayInstructions(0);
+        stepNumber = 0;
+    } else if (stepNumber != 0 & changeDirection == "Previous") {
+        displayInstructions(stepNumber - 1);
+        stepNumber = stepNumber - 1;
+    } else if (stepNumber != 5 & changeDirection == "Next") {
+        displayInstructions(stepNumber + 1);
+        stepNumber = stepNumber + 1;
+    }
+}
